dcd90e25e11dc2a8b91656598280cc8cafb59696
Daipayan0p <d.neogi.coding@gmail.com>
2025-05-18 19:24:48 +0530

    Initial Commit

---
+++ trackdown/trackdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackdownApp.swift
+//  trackdown
+//
+//  Created by Daipayan Neogi on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct trackdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
